7f96cfdba04f619c97950c7ecbace86ec26a3aba
Benjamin Bruch <benjamin.bruch@gmail.com>
2021-03-22 19:58:56 +0100

    [RENAMED] swift package name to old version (cause of resulting build error)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "RickMortySwiftAPI",
+    name: "rick-morty-swift-api",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13)
