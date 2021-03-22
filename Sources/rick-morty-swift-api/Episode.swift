de5ffd47714d385d4d4ce57280c22abb3f82bf49
Benjamin Bruch <benjamin.bruch@gmail.com>
2021-03-22 17:40:27 +0100

    [FIXED] episode model struct with coding keys to decode "air_date" property

--- Sources/rick-morty-swift-api/Episode.swift
+++ Sources/rick-morty-swift-api/Episode.swift
@@ -215,9 +215,17 @@ struct EpisodeInfoModel: Codable {
 public struct EpisodeModel: Codable, Identifiable {
     public let id: Int
     public let name: String
-    public let airDate: String = "air_date"
+    public let airDate: String
     public let episode: String
     public let characters: [String]
     public let url: String
     public let created: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, episode, characters, url, created
+        case airDate = "air_date"
+    }
 }
+
+
+
