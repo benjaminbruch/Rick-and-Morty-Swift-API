342f750caa8d9022a34f06a65ff8bb95cb0abdd1
Benjamin Bruch <benjamin.bruch@gmail.com>
2021-03-22 17:36:00 +0100

    [UPDATED] all character methods to combine future & promises including tests

--- Sources/rick-morty-swift-api/Character.swift
+++ Sources/rick-morty-swift-api/Character.swift
@@ -3,11 +3,12 @@
 //  Created by BBruch on 08.04.20.
 //
 
+import Combine
 import Foundation
 
 /**
-Character struct contains all functions to request character(s) information(s).
-*/
+ Character struct contains all functions to request character(s) information(s).
+ */
 public struct Character {
     
     public init(client: Client) {self.client = client}
@@ -18,18 +19,20 @@ public struct Character {
     /**
      Request character by id.
      - Parameters:
-        - id: ID of the character.
+     - id: ID of the character.
      - Returns: Character model struct.
      */
-    public func getCharacterByID(id: Int, completion: @escaping (Result<CharacterModel, Error>) -> Void) {
-        networkHandler.performAPIRequestByMethod(method: "character/"+String(id)) {
-            switch $0 {
-            case .success(let data):
-            if let character: CharacterModel = self.networkHandler.decodeJSONData(data: data) {
-                completion(.success(character))
-            }
-            case .failure(let error):
-            completion(.failure(error))
+    public func getCharacterByID(id: Int) -> Future <CharacterModel, Error> {
+        return Future() { promise in
+            networkHandler.performAPIRequestByMethod(method: "character/"+String(id)) {
+                switch $0 {
+                case .success(let data):
+                    if let character: CharacterModel = self.networkHandler.decodeJSONData(data: data) {
+                        promise(.success(character))
+                    }
+                case .failure(let error):
+                    promise(.failure(error))
+                }
             }
         }
     }
@@ -37,38 +40,41 @@ public struct Character {
     /**
      Request character by URL.
      - Parameters:
-        - url: URL of the character.
+     - url: URL of the character.
      - Returns: Character model struct.
      */
-    public func getCharacterByURL(url: String, completion: @escaping (Result<CharacterModel, Error>) -> Void) {
-        networkHandler.performAPIRequestByURL(url: url) {
-            switch $0 {
-            case .success(let data):
-            if let character: CharacterModel = self.networkHandler.decodeJSONData(data: data) {
-            completion(.success(character))
-            }
-            case .failure(let error):
-            completion(.failure(error))
+    public func getCharacterByURL(url: String) -> Future <CharacterModel, Error> {
+        return Future() { promise in
+            networkHandler.performAPIRequestByURL(url: url) {
+                switch $0 {
+                case .success(let data):
+                    if let character: CharacterModel = self.networkHandler.decodeJSONData(data: data) {
+                        promise(.success(character))
+                    }
+                case .failure(let error):
+                    promise(.failure(error))
+                }
             }
         }
     }
-    
     /**
      Request multiple characters by IDs.
-    - Parameters:
-        - ids: Character ids.
+     - Parameters:
+     - ids: Character ids.
      - Returns: Array of Character model struct.
      */
-    public func getCharactersByID(ids: [Int], completion: @escaping (Result<[CharacterModel], Error>) -> Void) {
-        let stringIDs = ids.map { String($0) }
-        networkHandler.performAPIRequestByMethod(method: "character/"+stringIDs.joined(separator: ",")) {
-            switch $0 {
-            case .success(let data):
-            if let characters: [CharacterModel] = self.networkHandler.decodeJSONData(data: data) {
-                completion(.success(characters))
-            }
-            case .failure(let error):
-            completion(.failure(error))
+    public func getCharactersByID(ids: [Int]) -> Future <[CharacterModel], Error> {
+        return Future() { promise in
+            let stringIDs = ids.map { String($0) }
+            networkHandler.performAPIRequestByMethod(method: "character/"+stringIDs.joined(separator: ",")) {
+                switch $0 {
+                case .success(let data):
+                    if let characters: [CharacterModel] = self.networkHandler.decodeJSONData(data: data) {
+                        promise(.success(characters))
+                    }
+                case .failure(let error):
+                    promise(.failure(error))
+                }
             }
         }
     }
@@ -76,18 +82,20 @@ public struct Character {
     /**
      Request characters by page number.
      - Parameters:
-        - page: Number of result page.
+     - page: Number of result page.
      - Returns: Array of Character model struct.
      */
-    public func getCharactersByPageNumber(pageNumber: Int, completion: @escaping (Result<[CharacterModel], Error>) -> Void) {
-        networkHandler.performAPIRequestByMethod(method: "character/"+"?page="+String(pageNumber)) {
-            switch $0 {
-            case .success(let data):
-            if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
-            completion(.success(infoModel.results))
-            }
-            case .failure(let error):
-            completion(.failure(error))
+    public func getCharactersByPageNumber(pageNumber: Int) -> Future <[CharacterModel], Error> {
+        return Future() { promise in
+            networkHandler.performAPIRequestByMethod(method: "character/"+"?page="+String(pageNumber)) {
+                switch $0 {
+                case .success(let data):
+                    if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
+                        promise(.success(infoModel.results))
+                    }
+                case .failure(let error):
+                    promise(.failure(error))
+                }
             }
         }
     }
@@ -96,45 +104,49 @@ public struct Character {
      Request all characters.
      - Returns: Array of Character model struct.
      */
-    public func getAllCharacters(completion: @escaping (Result<[CharacterModel], Error>) -> Void) {
-        var allCharacters = [CharacterModel]()
-        networkHandler.performAPIRequestByMethod(method: "character") {
-            switch $0 {
-            case .success(let data):
-            if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
-                allCharacters = infoModel.results
-                let charactersDispatchGroup = DispatchGroup()
-                
-                for index in 2...infoModel.info.pages {
-                    charactersDispatchGroup.enter()
-                    self.getCharactersByPageNumber(pageNumber: index) {
-                        switch $0 {
-                        case .success(let characters):
-                        allCharacters.append(contentsOf:characters)
-                        charactersDispatchGroup.leave()
-                        case .failure(let error):
-                        completion(.failure(error))
+    public func getAllCharacters() -> Future <[CharacterModel], Error> {
+        return Future() { promise in
+            var allCharacters = [CharacterModel]()
+            networkHandler.performAPIRequestByMethod(method: "character") {
+                switch $0 {
+                case .success(let data):
+                    if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
+                        allCharacters = infoModel.results
+                        let charactersDispatchGroup = DispatchGroup()
+                        
+                        for index in 2...infoModel.info.pages {
+                            charactersDispatchGroup.enter()
+                            
+                            networkHandler.performAPIRequestByMethod(method: "character/"+"?page="+String(index)) {
+                                switch $0 {
+                                case .success(let data):
+                                    if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
+                                        allCharacters.append(contentsOf: infoModel.results)
+                                        charactersDispatchGroup.leave()
+                                    }
+                                case .failure(let error):
+                                    promise(.failure(error))
+                                }
+                            }
+                        }
+                        charactersDispatchGroup.notify(queue: DispatchQueue.main) {
+                            promise(.success(allCharacters.sorted { $0.id < $1.id }))
                         }
                     }
+                case .failure(let error):
+                    promise(.failure(error))
                 }
-                charactersDispatchGroup.notify(queue: DispatchQueue.main) {
-                    completion(.success(allCharacters.sorted { $0.id < $1.id }))
-                }
-            }
-            case .failure(let error):
-            completion(.failure(error))
             }
         }
     }
-    
     /**
      Create character filter with given parameters.
      - Parameters:
-        - name: The name of the character.
-        - status: The species of the character.
-        - species: The species of the character.
-        - type: The type or subspecies of the character.
-        - gender: The species of the character.
+     - name: The name of the character.
+     - status: The species of the character.
+     - species: The species of the character.
+     - type: The type or subspecies of the character.
+     - gender: The species of the character.
      - Returns: CharacterFilter
      */
     func createCharacterFilter(name: String?, status: Status?, species: String?, type: String?, gender: Gender?) -> CharacterFilter {
@@ -161,19 +173,21 @@ public struct Character {
     /**
      Request characters with given filter.
      - Parameters:
-        - filter: CharacterFilter struct (provides requestURL with query options).
+     - filter: CharacterFilter struct (provides requestURL with query options).
      - Returns: Array of Character model struct.
      */
-    public func getCharactersByFilter(filter: CharacterFilter, completion: @escaping (Result<[CharacterModel], Error>) -> Void) {
-        
-        networkHandler.performAPIRequestByMethod(method: filter.query) {
-            switch $0 {
-            case .success(let data):
-            if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
-                completion(.success(infoModel.results))
-            }
-            case .failure(let error):
-            completion(.failure(error))
+    public func getCharactersByFilter(filter: CharacterFilter) -> Future<[CharacterModel], Error> {
+        return Future() { promise in
+            
+            networkHandler.performAPIRequestByMethod(method: filter.query) {
+                switch $0 {
+                case .success(let data):
+                    if let infoModel: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
+                        promise(.success(infoModel.results))
+                    }
+                case .failure(let error):
+                    promise(.failure(error))
+                }
             }
         }
     }
@@ -183,12 +197,12 @@ public struct Character {
 /**
  Struct to store character filter properties.
  ### Properties
-    - **name**: The name of the character.
-    - **status**: The species of the character.
-    - **species**: The species of the character.
-    - **type**: The type or subspecies of the character.
-    - **gender**: The species of the character.
-    - **query**: URL query for HTTP request.
+ - **name**: The name of the character.
+ - **status**: The species of the character.
+ - **species**: The species of the character.
+ - **type**: The type or subspecies of the character.
+ - **gender**: The species of the character.
+ - **query**: URL query for HTTP request.
  */
 public struct CharacterFilter {
     public let name: String
@@ -202,8 +216,8 @@ public struct CharacterFilter {
 /**
  CharacterInfoModel struct for decoding info json response.
  ### Properties
-    - **info**: Information about characters count and pagination.
-    - **results**: First page with 20 characters.
+ - **info**: Information about characters count and pagination.
+ - **results**: First page with 20 characters.
  
  ### SeeAlso
  - **Info**: Info struct in Network.swift.
@@ -217,18 +231,18 @@ struct CharacterInfoModel: Codable {
 /**
  Character struct for decoding character json response.
  ### Properties
-    - **id**: The id of the character.
-    - **name**: The name of the character.
-    - **status**: The status of the character ('Alive', 'Dead' or 'unknown').
-    - **species**: The species of the character.
-    - **type**: The type or subspecies of the character.
-    - **gender**: The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
-    - **origin**: Name and link to the character's origin location.
-    - **location**: Name and link to the character's last known location endpoint.
-    - **image**: Link to the character's image. All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
-    - **episodes**: List of episodes in which this character appeared.
-    - **url**: Link to the character's own URL endpoint.
-    - **created**: Time at which the character was created in the database.
+ - **id**: The id of the character.
+ - **name**: The name of the character.
+ - **status**: The status of the character ('Alive', 'Dead' or 'unknown').
+ - **species**: The species of the character.
+ - **type**: The type or subspecies of the character.
+ - **gender**: The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
+ - **origin**: Name and link to the character's origin location.
+ - **location**: Name and link to the character's last known location endpoint.
+ - **image**: Link to the character's image. All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
+ - **episodes**: List of episodes in which this character appeared.
+ - **url**: Link to the character's own URL endpoint.
+ - **created**: Time at which the character was created in the database.
  */
 public struct CharacterModel: Codable, Identifiable {
     public let id: Int
@@ -248,8 +262,8 @@ public struct CharacterModel: Codable, Identifiable {
 /**
  Origin struct for decoding character json origin response.
  ### Properties
-    - **name**: The name of the origin.
-    - **url**: Link to the origin's own URL endpoint.
+ - **name**: The name of the origin.
+ - **url**: Link to the origin's own URL endpoint.
  */
 public struct CharacterOriginModel: Codable {
     public let name: String
@@ -259,8 +273,8 @@ public struct CharacterOriginModel: Codable {
 /**
  Location struct for decoding character location json response.
  ### Properties
-    - **name**: The name of the location.
-    - **url**: Link to the location's own URL endpoint.
+ - **name**: The name of the location.
+ - **url**: Link to the location's own URL endpoint.
  */
 public struct CharacterLocationModel: Codable {
     public let name: String
