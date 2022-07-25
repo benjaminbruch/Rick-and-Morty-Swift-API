adf680a3459d50aa7e3db9a6954033ade4f6adc0
Benjamin Bruch <benjamin.bruch@gmail.com>
2022-07-25 23:58:28 +0200

    [UPDATED] minimum platform requirements & swift tools version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RickMortySwiftApi",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v13)
     ],
     products: [
